b02fd601acb14eb28e339112f941599ad275dec7
Bhushan <alhatbhushan@gmail.com>
2024-12-21 23:33:08 +0530

    [frontend] feat: allow changing user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,29 @@ function App() {
 				<p>Roles: {user.roles.join(', ')}</p>
 			</div>
 
-			<div className="mx-auto grid w-1/2 grid-cols-2 gap-4">
+			<div className="flex items-center gap-4">
+				<div className="w-20">User</div>
+				{users.map((user) => (
+					<div>
+						<input
+							type="radio"
+							id={user.id.toString()}
+							name="hosting"
+							value={user.id.toString()}
+							className="peer hidden"
+							onChange={() => setUser((prev) => ({ ...prev, id: user.id, name: user.name }))}
+						/>
+						<label
+							htmlFor={user.id.toString()}
+							className="inline-flex cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600"
+						>
+							{user.name}
+						</label>
+					</div>
+				))}
+			</div>
+
+			<div className="mx-auto mt-12 grid w-1/2 grid-cols-2 gap-4">
 				{todos.map((todo) => (
 					<div key={todo.id} className="rounded-md border border-gray-300 p-4">
 						<p>{todo.title}</p>
@@ -45,4 +67,12 @@ const todos = [
 	{ id: 4, title: 'Todo 4', completed: true, createdBy: 2 },
 	{ id: 5, title: 'Todo 5', completed: false, createdBy: 3 },
 	{ id: 6, title: 'Todo 6', completed: true, createdBy: 3 },
+];
+
+const users = [
+	{ id: 1, name: 'Leanne Graham' },
+	{ id: 2, name: 'Ervin Howell' },
+	{ id: 3, name: 'Clementine Bauch' },
+	{ id: 4, name: 'Patricia Lebsack' },
+	{ id: 5, name: 'Chelsey Dietrich' },
 ];
