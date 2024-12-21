f27c91cf6bbaf6f29bbb21483886cf4ddff610a3
Bhushan <alhatbhushan@gmail.com>
2024-12-21 22:57:37 +0530

    [frontned] chore: setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+function App() {
+	return (
+		<main className="container py-8">
+			<Button>Click me</Button>
+		</main>
+	);
+}
+
+export default App;
