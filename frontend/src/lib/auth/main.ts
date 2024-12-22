3f67643434fdbb1942e52ccfda0b01dfe6aedf36
Bhushan <alhatbhushan@gmail.com>
2024-12-22 17:25:01 +0530

    [frontend] feat: handle advanced conditions

--- frontend/src/lib/auth/main.ts
+++ frontend/src/lib/auth/main.ts
@@ -25,10 +25,15 @@ export type Resource = {
 	attributes: Attributes;
 };
 
+export type AdvancedCondition =
+	| { value: string | number | boolean; operator: 'eq' | 'ne' }
+	| { value: number; operator: 'gt' | 'gte' | 'lt' | 'lte' }
+	| { value: unknown[]; operator: 'in' | 'nin' };
+
 export type Policy = {
 	action: Action;
 	resource: ResourceType;
-	conditions: Attributes;
+	conditions: Record<string, AdvancedCondition>;
 };
 
 export class Auth {
@@ -42,6 +47,101 @@ export class Auth {
 		if (this.abac(user, action, resource)) return true;
 
 		return false;
+	}
+
+	private rbac(user: User, action: Action) {
+		const isRoleAuthorized = user.roles.some((role) =>
+			role.permissions.includes(action)
+		);
+		if (isRoleAuthorized) return true;
+
+		return false;
+	}
+
+	private abac(user: User, action: Action, resource: Resource) {
+		const relevantPolicies = this.policies.filter((policy) => {
+			return policy.resource === resource.type && policy.action === action;
+		});
+
+		for (const policy of relevantPolicies) {
+			const isPolicyAuthorized = this.evaluatePolicy(policy, user, resource);
+			if (isPolicyAuthorized) return true;
+		}
+
+		return false;
+	}
+
+	private evaluatePolicy(policy: Policy, user: User, resource: Resource) {
+		for (const key in policy.conditions) {
+			if (!resource.attributes[key] || !user.attributes[key]) {
+				continue;
+			}
+
+			const condition = policy.conditions[key];
+			const resourceValue = resource.attributes[key];
+			const userValue = user.attributes[key];
+
+			const resourceEval = this.evaluateCondition(condition, resourceValue);
+			const userEval = this.evaluateCondition(condition, userValue);
+
+			if (resourceEval && userEval) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private evaluateCondition(condition: AdvancedCondition, value: unknown) {
+		function isPrimitive(value: unknown) {
+			return (
+				typeof value === 'string' ||
+				typeof value === 'number' ||
+				typeof value === 'boolean'
+			);
+		}
+
+		function validateValue(check: boolean, operator: string, value: unknown) {
+			if (!check) {
+				throw new InvalidOperandError(value, operator);
+			}
+		}
+
+		switch (condition.operator) {
+			case 'eq':
+			case 'ne': {
+				validateValue(isPrimitive(value), condition.operator, value);
+				const val = value as string | number | boolean;
+				return condition.operator === 'eq'
+					? val === condition.value
+					: val !== condition.value;
+			}
+
+			case 'gt':
+			case 'gte':
+			case 'lt':
+			case 'lte': {
+				validateValue(typeof value === 'number', condition.operator, value);
+				const val = value as number;
+				switch (condition.operator) {
+					case 'gt':
+						return val > condition.value;
+					case 'gte':
+						return val >= condition.value;
+					case 'lt':
+						return val < condition.value;
+					case 'lte':
+						return val <= condition.value;
+				}
+			}
+
+			case 'in':
+			case 'nin': {
+				validateValue(Array.isArray(value), condition.operator, value);
+				return condition.operator === 'in'
+					? condition.value.includes(value)
+					: !condition.value.includes(value);
+			}
+		}
 	}
 
 	// this can later be replaced with zod
@@ -60,36 +160,10 @@ export class Auth {
 			throw new Error(`Invalid resource: ${policy.resource}`);
 		}
 	}
+}
 
-	private rbac(user: User, action: Action) {
-		const isRoleAuthorized = user.roles.some((role) =>
-			role.permissions.includes(action)
-		);
-		if (isRoleAuthorized) return true;
-
-		return false;
-	}
-
-	private abac(user: User, action: Action, resource: Resource) {
-		const relevantPolicies = this.policies.filter((policy) => {
-			return policy.resource === resource.type && policy.action === action;
-		});
-
-		for (const policy of relevantPolicies) {
-			for (const key in policy.conditions) {
-				if (!resource.attributes[key] || !user.attributes[key]) {
-					continue;
-				}
-
-				if (
-					resource.attributes[key] === policy.conditions[key] &&
-					user.attributes[key] === policy.conditions[key]
-				) {
-					return true;
-				}
-			}
-		}
-
-		return false;
+class InvalidOperandError extends Error {
+	constructor(value: unknown, operator: string) {
+		super(`Invalid value type: ${typeof value} for condition ${operator}`);
 	}
 }
--- frontend/src/lib/auth/values.ts
+++ frontend/src/lib/auth/values.ts
@@ -26,28 +26,51 @@ const doc2: Resource = {
 	},
 };
 
+const file1: Resource = {
+	id: '1',
+	type: 'file',
+	attributes: {
+		department: 'finance',
+	},
+};
+
 const adminUser: User = {
 	id: '1',
 	roles: [admin],
 	attributes: {},
 };
 
-const userUser: User = {
+const userUser_1: User = {
 	id: '2',
 	roles: [user],
 	attributes: { department: 'engineering' },
+};
+
+const userUser_2: User = {
+	id: '3',
+	roles: [user],
+	attributes: { department: 'engineering', level: 6 },
 };
 
 const policies: Policy[] = [
 	{
 		action: 'read',
 		resource: 'document',
-		conditions: { department: 'engineering' },
+		conditions: { department: { operator: 'eq', value: 'engineering' } },
 	},
 	{
 		action: 'update',
 		resource: 'document',
-		conditions: { department: 'engineering' },
+		// any one of the conditions must be true
+		conditions: {
+			department: { operator: 'eq', value: 'engineering' },
+			level: { operator: 'gt', value: 10 },
+		},
+	},
+	{
+		action: 'delete',
+		resource: 'file',
+		conditions: { level: { operator: 'gt', value: 5 } },
 	},
 ];
 
@@ -62,18 +85,32 @@ const tests = [
 
 	() =>
 		console.assert(
-			auth.isAuthorized(userUser, doc1, 'update'),
+			auth.isAuthorized(userUser_1, doc1, 'update'),
 			'user in engineering should be authorized to update document in engineering'
 		),
 
 	() =>
 		console.assert(
-			!auth.isAuthorized(userUser, doc2, 'update'),
+			auth.isAuthorized(userUser_1, doc1, 'update'),
 			'user in engineering should not be authorized to update document in finance'
+		),
+
+	() =>
+		console.assert(
+			!auth.isAuthorized(userUser_1, file1, 'delete'),
+			'user with no level should not be authorized to delete file'
+		),
+
+	// this fails because the attribute might be missing from the resource
+	() =>
+		console.assert(
+			auth.isAuthorized(userUser_2, file1, 'delete'),
+			'user with level 6 should be authorized to delete file level 5'
 		),
 ];
 
 tests.forEach((test, i) => {
+	console.log('-------------------');
 	console.log(`Test ${i + 1}`);
 	test();
 });
