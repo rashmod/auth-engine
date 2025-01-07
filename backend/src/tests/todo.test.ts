ede67398e903372e0371c32cac66b6b0cbd5f5eb
Bhushan <alhatbhushan@gmail.com>
2025-01-07 15:11:26 +0530

    [backend] feat: added test for no policy case

--- backend/src/tests/todo.test.ts
+++ backend/src/tests/todo.test.ts
@@ -4,10 +4,10 @@ import { Auth } from '@/engine';
 import { PolicyManager } from '@/policy-manager';
 
 describe('Basic todo app', () => {
-	const resources = ['user', 'todo'] as const;
+	const resources = ['user', 'todo', 'file'] as const;
 
-	const policyGenerator = new PolicyManager(resources);
-	policyGenerator.addPolicies([
+	const policyManager = new PolicyManager(resources);
+	policyManager.addPolicies([
 		{
 			action: 'create',
 			resource: 'todo',
@@ -60,77 +60,113 @@ describe('Basic todo app', () => {
 				],
 			},
 		},
+		{
+			action: 'read',
+			resource: 'file',
+		},
 	]);
 
-	const policies = policyGenerator.getPolicies();
+	const policies = policyManager.getPolicies();
 
 	const auth = new Auth(policies);
 
-	const user = policyGenerator.createResource({
+	const user = policyManager.createResource({
 		id: 'user1',
 		type: 'user',
 		attributes: { role: 'user', id: 'user1' },
 	});
 
-	const anotherUser = policyGenerator.createResource({
+	const anotherUser = policyManager.createResource({
 		id: 'user2',
 		type: 'user',
 		attributes: { role: 'user' },
 	});
 
-	const admin = policyGenerator.createResource({
+	const admin = policyManager.createResource({
 		id: 'admin1',
 		type: 'user',
 		attributes: { role: 'admin' },
 	});
 
-	const todo = policyGenerator.createResource({
+	const todo = policyManager.createResource({
 		id: 'todo1',
 		type: 'todo',
 		attributes: { ownerId: 'user1' },
 	});
 
-	it('should allow user to create a todo', () => {
-		expect(auth.isAuthorized(user, todo, 'create')).toBe(true);
+	describe('create todo', () => {
+		it('should allow user to create a todo', () => {
+			expect(auth.isAuthorized(user, todo, 'create')).toBe(true);
+		});
+
+		it('should allow admin to create a todo', () => {
+			expect(auth.isAuthorized(admin, todo, 'create')).toBe(true);
+		});
 	});
 
-	it('should allow admin to create a todo', () => {
-		expect(auth.isAuthorized(admin, todo, 'create')).toBe(true);
+	describe('read todo', () => {
+		it('should allow user to read a todo', () => {
+			expect(auth.isAuthorized(user, todo, 'read')).toBe(true);
+		});
+
+		it('should allow admin to read a todo', () => {
+			expect(auth.isAuthorized(admin, todo, 'read')).toBe(true);
+		});
 	});
 
-	it('should allow user to read a todo', () => {
-		expect(auth.isAuthorized(user, todo, 'read')).toBe(true);
+	describe('update todo', () => {
+		it('should allow other user to read a todo', () => {
+			expect(auth.isAuthorized(user, todo, 'read')).toBe(true);
+		});
+
+		it('should allow owner to update a todo', () => {
+			expect(auth.isAuthorized(user, todo, 'update')).toBe(true);
+		});
+
+		it('should allow admin to update a todo', () => {
+			expect(auth.isAuthorized(admin, todo, 'update')).toBe(true);
+		});
+
+		it('should not allow other user to update a todo', () => {
+			expect(auth.isAuthorized(anotherUser, todo, 'update')).toBe(false);
+		});
 	});
 
-	it('should allow admin to read a todo', () => {
-		expect(auth.isAuthorized(admin, todo, 'read')).toBe(true);
+	describe('delete todo', () => {
+		it('should allow owner to delete a todo', () => {
+			expect(auth.isAuthorized(user, todo, 'delete')).toBe(true);
+		});
+
+		it('should allow admin to delete a todo', () => {
+			expect(auth.isAuthorized(admin, todo, 'delete')).toBe(true);
+		});
+
+		it('should not allow other user to delete a todo', () => {
+			expect(auth.isAuthorized(anotherUser, todo, 'delete')).toBe(false);
+		});
 	});
 
-	it('should allow other user to read a todo', () => {
-		expect(auth.isAuthorized(user, todo, 'read')).toBe(true);
-	});
+	describe('no policy', () => {
+		const file = policyManager.createResource({
+			type: 'file',
+			id: 'file1',
+			attributes: {},
+		});
 
-	it('should allow owner to update a todo', () => {
-		expect(auth.isAuthorized(user, todo, 'update')).toBe(true);
-	});
+		it('should not allow to create file', () => {
+			expect(auth.isAuthorized(user, file, 'create')).toBe(false);
+		});
 
-	it('should allow admin to update a todo', () => {
-		expect(auth.isAuthorized(admin, todo, 'update')).toBe(true);
-	});
+		it('should allow to read file', () => {
+			expect(auth.isAuthorized(user, file, 'read')).toBe(true);
+		});
 
-	it('should not allow other user to update a todo', () => {
-		expect(auth.isAuthorized(anotherUser, todo, 'update')).toBe(false);
-	});
+		it('should not allow to update file', () => {
+			expect(auth.isAuthorized(user, file, 'update')).toBe(false);
+		});
 
-	it('should allow owner to delete a todo', () => {
-		expect(auth.isAuthorized(user, todo, 'delete')).toBe(true);
-	});
-
-	it('should allow admin to delete a todo', () => {
-		expect(auth.isAuthorized(admin, todo, 'delete')).toBe(true);
-	});
-
-	it('should not allow other user to delete a todo', () => {
-		expect(auth.isAuthorized(anotherUser, todo, 'delete')).toBe(false);
+		it('should not allow to delete file', () => {
+			expect(auth.isAuthorized(user, file, 'delete')).toBe(false);
+		});
 	});
 });
